08fbcecd49b4b218bb7bfdd39a5882bfaca3def6
Nikola <nikola88393@gmail.com>
2023-08-15 12:11:54 +0300

    Finis 05 exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(numStart, numEnd) {
+    if(typeof numStart !== 'number' || typeof numEnd != 'number'){
+        return "ERROR";
+    }
+    if(numStart < 0 || numEnd < 0){
+        return "ERROR";
+    }
+    let sum = 0;
+    if(numStart < numEnd){
+    for(let i = numStart; i < numEnd + 1; i++){
+        sum += i;
+    }
+}
+else {
+    for(let i = numEnd; i < numStart + 1; i++){
+        sum += i;
+    }
+}
+    return sum;
 };
 
+sumAll(50,1);
 // Do not edit below this line
 module.exports = sumAll;
