07ab7c1529534ebf441ded6132aa9335d3842c17
Nikola <nikola88393@gmail.com>
2023-08-15 11:31:56 +0300

    Finish 03 exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    if(string === null){
+        return "ERROR";
+    }
+    let reversed = "";
+    for(let i = string.length - 1; i >= 0; i--){
+        reversed += string[i];
+    }
+    return reversed;
 };
 
 // Do not edit below this line
