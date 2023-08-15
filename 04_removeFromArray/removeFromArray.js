e67fc0deb1082dfd8714feb8d4952c480905e723
Nikola <nikola88393@gmail.com>
2023-08-15 11:51:12 +0300

    Finish 04 exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    args.forEach((arg) => {
+    for(let i = 0; i < array.length; i++){
+        if(array[i] === arg){
+            array.splice(i, 1);
+        }
+    }
+});
+return array;
 };
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
