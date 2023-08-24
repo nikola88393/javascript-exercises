97b3af0de13d0d68962f3a83b1c5795f9630f860
Nikola <nikola88393@gmail.com>
2023-08-24 15:40:39 +0300

    Finish exercises 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let newString = string.replace(/[\s.,\/#!$%\^&\*;:{}=\-_`~()]/g, '').toLowerCase(); // Corrected: Replace all spaces and convert to lowercase
+    let newStringReversed = newString.split("").reverse().join("");
+    if(newString === newStringReversed){
+        return true;
+    }
+    else{
+        return false;
+    }
 };
-
+palindromes('Animal loots foliated detail of stool lamina.')
 // Do not edit below this line
 module.exports = palindromes;
