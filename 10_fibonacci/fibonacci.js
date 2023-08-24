7c08e740379b7406434f684c10cb12f5b6fb939c
Nikola <nikola88393@gmail.com>
2023-08-24 15:51:46 +0300

    Finish exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    parseInt(n);
+    if(n < 0){
+        return "OOPS";
+    }
+    let arr = [1, 1];
 
+    for(let i = 0; i < n - 1; i++){
+        arr.push(arr[i] + arr[i+1]);
+    }
+
+    return arr[n - 1];
 };
+
+console.log(fibonacci(10));
 
 // Do not edit below this line
 module.exports = fibonacci;
