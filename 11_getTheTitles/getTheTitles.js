8d987d0f2a709aa98a5dec569e9f34a2ed0544de
Nikola <nikola88393@gmail.com>
2023-08-24 16:02:36 +0300

    Finish exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,4 +1,10 @@
-const getTheTitles = function() {
+const getTheTitles = function(obj) {
+    let books = [];
+    obj.forEach(element => {
+        books.push(element.title);
+    });
+
+    return books;
 
 };
 
