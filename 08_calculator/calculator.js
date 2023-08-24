bda9456cfd5a5afc1bc62a5c27ada18991734aa3
Nikola <nikola88393@gmail.com>
2023-08-24 15:04:00 +0300

    Finish exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return parseInt(a) + parseInt(b);
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+	let sum = 0;
+  for(let i = 0; i < a.length; i++){
+    sum += a[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  let multi = 1;
+  for(let i = 0; i < a.length; i++){
+    multi *= a[i];
+  }
+  return multi;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if(a > 0){
+    return a * factorial(a - 1);
+  }
+  else{
+    return 1;
+  }
 };
 
 // Do not edit below this line
